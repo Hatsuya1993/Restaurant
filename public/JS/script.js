be5765d149e476e3d8295132fc218c4b055a206d
Hatsuya1993 <60688830+Hatsuya1993@users.noreply.github.com>
2020-07-23 07:25:50 +0800

    nav2 scroll done

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
-    $(".banner").ripples({
-        dropRadius: 20,
-        perturbance: 0.03
-    });
-})
+  $(window).scroll(() => {
+    let pos = $(this).scrollTop();
+    if (pos >= 483.20001220703125) {
+      $("#nav2").addClass("fixed-top");
+    } else {
+      $("#nav2").removeClass("fixed-top");
+    }
+  });
+
+  // Ripples
+  $(".banner").ripples({
+    dropRadius: 20,
+    perturbance: 0.03,
+  });
+});
 
 $(document).ready(function () {
-    $('.button-edit').magnificPopup({
-        type: 'image'
-    });
+  $(".button-edit").magnificPopup({
+    type: "image",
+  });
 });
